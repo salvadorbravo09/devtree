4b3ff27bbb33b8de33f572d033243f31fbc36558
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-29 17:52:31 -0300

    feat: initialize Express server with basic routing

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.get("/", (req, res) => {
 });
 
 // Port
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port: ${port}`);
 });
